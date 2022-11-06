540853eb339bf70dad1d8b6f368def5725539efd
truthsoloud <truthsoloud@gachon.ac.kr>
2022-11-06 18:46:04 +0900

    week2 - flush

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -19,11 +19,12 @@ public class JpaMain {
         try {
 
             //영속
-            Member member = em.find(Member.class, 150L);
-            member.setName("ZZZZZ");
+            Member member = new Member(200L, "member200");
+            em.persist(member);
+
+            em.flush();
 
             System.out.println("======================");
-
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
