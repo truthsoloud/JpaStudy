8bbd2a06c3a23827a7fa67e21750c79ceb5b6510
truthsoloud <truthsoloud@gachon.ac.kr>
2022-11-06 18:07:27 +0900

    week2 - verifying that the query is processed after commit

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -19,10 +19,12 @@ public class JpaMain {
         try {
 
             //영속
-            Member findMember1 = em.find(Member.class, 101L);
-            Member findMember2 = em.find(Member.class, 101L);
+            Member member1 = new Member(150L, "A");
+            Member member2 = new Member(160L, "B");
 
-            System.out.println("result = " + (findMember1 == findMember2));
+            em.persist(member1);
+            em.persist(member2);
+            System.out.println("======================"); //이 선 아래로 쿼리가 나감! 커밋이 완료된 이후에 쿼리가 날아간다는 걸 알 수 있따.
 
             tx.commit();
         } catch (Exception e) {
