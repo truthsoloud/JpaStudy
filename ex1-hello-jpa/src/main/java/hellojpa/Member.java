a2fdbbe8462f16338b7b8698638c2f09cbd80266
truthsoloud <truthsoloud@gachon.ac.kr>
2022-12-20 14:15:45 +0900

    It's better to use the string type than int type when you use the Roletype column

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,6 +1,7 @@
 package hellojpa;
 
 import javax.persistence.*;
+import java.math.BigDecimal;
 import java.util.Date;
 
 @Entity
@@ -12,10 +13,11 @@ public class Member {
 
     //객체는 username이라고 쓰고 싶고 db에는 name이라고 쓰고 싶을 때
     //db column명은 name
-    @Column(name = "name", nullable = false, columnDefinition = "varchar(100) defualt 'EMPTY'") //not null 제약조건
+    @Column(name = "name", nullable = false, columnDefinition = "varchar(100) default 'EMPTY'") //not null 제약조건
     private String username;
 
-    private Integer age;
+    @Column()
+    private BigDecimal age; //숫자가 매우 큰 경우, 소숫점 써야하는 경우
 
     @Enumerated(EnumType.STRING)
     private RoleType roleType;
@@ -34,5 +36,69 @@ public class Member {
     private int temp;
 
     public Member() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public BigDecimal getAge() {
+        return age;
+    }
+
+    public void setAge(BigDecimal age) {
+        this.age = age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getTemp() {
+        return temp;
+    }
+
+    public void setTemp(int temp) {
+        this.temp = temp;
     }
 }
