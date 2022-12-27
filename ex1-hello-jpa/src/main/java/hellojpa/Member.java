bdb2251c9e83e6fa1d33fa81554881923a72e270
truthsoloud <truthsoloud@gachon.ac.kr>
2022-12-27 14:00:56 +0900

    Sequence strategy - mapping

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -5,35 +5,15 @@ import java.math.BigDecimal;
 import java.util.Date;
 
 @Entity
+@SequenceGenerator(name = "member_seq_generator", sequenceName = "member_seq")
 public class Member {
 
-    //pk매핑
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_generator")
     private Long id;
 
-    //객체는 username이라고 쓰고 싶고 db에는 name이라고 쓰고 싶을 때
-    //db column명은 name
-    @Column(name = "name", nullable = false, columnDefinition = "varchar(100) default 'EMPTY'") //not null 제약조건
+    @Column(name = "name", nullable = false)
     private String username;
-
-    @Column()
-    private BigDecimal age; //숫자가 매우 큰 경우, 소숫점 써야하는 경우
-
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-
-    //db에 varchar를 넘어서는 큰 컨텐츠를 넣고 싶을 때 Lob사용
-    @Lob
-    private String description;
-
-    @Transient
-    private int temp;
 
     public Member() {
     }
@@ -52,53 +32,5 @@ public class Member {
 
     public void setUsername(String username) {
         this.username = username;
-    }
-
-    public BigDecimal getAge() {
-        return age;
-    }
-
-    public void setAge(BigDecimal age) {
-        this.age = age;
-    }
-
-    public RoleType getRoleType() {
-        return roleType;
-    }
-
-    public void setRoleType(RoleType roleType) {
-        this.roleType = roleType;
-    }
-
-    public Date getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public Date getLastModifiedDate() {
-        return lastModifiedDate;
-    }
-
-    public void setLastModifiedDate(Date lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public int getTemp() {
-        return temp;
-    }
-
-    public void setTemp(int temp) {
-        this.temp = temp;
     }
 }
