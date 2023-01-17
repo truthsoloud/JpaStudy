0324a52e1d2482cee3cd7c6be69764f2a8fe345f
truthsoloud <truthsoloud@gachon.ac.kr>
2023-01-17 14:19:00 +0900

    to get data from the DB, not from the persistence context

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -28,6 +28,11 @@ public class JpaMain {
             member.setTeam(team);
             em.persist(member);
 
+            //영속성 컨텍스트 말고 DB에서 가져오는 걸 보고 싶다면
+            em.flush(); //강제 호출
+            em.clear(); //영속성 컨텍스트 초기화
+
+
             Member findMember = em.find(Member.class, member.getId());
 
             Team findTeam = findMember.getTeam();
