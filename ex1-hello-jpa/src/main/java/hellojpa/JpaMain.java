709ecc8d5e0cdb705cd5ffde0d49d798ce3efe87
truthsoloud <truthsoloud@gachon.ac.kr>
2023-03-08 19:45:00 +0900

    various relationship Mapping: table per implemetation class

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -17,19 +17,20 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setUsername("member1");
 
-            em.persist(member);
+            Movie movie = new Movie();
+            movie.setDirector("박찬욱");
+            movie.setActor("탕웨이");
+            movie.setName("헤어질 결심");
+            movie.setPrice(17000);
 
-            Team team = new Team();
-            team.setName("teamA");
+            em.persist(movie);
 
-            //team엔티티를 저장하는데 옆 테이블에 가서 update할 수 밖에 없음. 어쩔 수 없이 쿼리가 하나 더 나가야하는 부분.
-            //이 구조의 또 하나의 문제점: 실무에는 테이블이 굉장히 많으므로 운영이 힘들어진다.
-            team.getMembers().add(member);
+            em.flush();
+            em.clear();
 
-            em.persist(team);
+            Movie findMovie = em.find(Movie.class, movie.getId());
+            System.out.println("findMovie =" + findMovie);
 
             tx.commit();
         } catch (Exception e) {
